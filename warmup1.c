0defd38a7e7ba03ee9bda7acf0a1c83e623852ee
Apollo <jackratermann@gmail.com>
2022-06-10 15:57:46 -0700

    Add files via upload

---
+++ warmup1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void StartPoint();
+void C1();
+void C2();
+void C3();
+int main()
+{
+    int age = 0;
+    printf("What is your age: \n");
+    scanf("%d", &age);
+    printf("Your age is %d in eldritch years. \n", age*50);
+    printf("\n");
+    StartPoint();
+}
+
+void StartPoint()
+{
+    int choice = 0;
+    printf("Good. Now that is out of the way. You are in a dark room. \n");
+    printf("What would you like to do? \n");
+    printf("1. Walk Around | 2. Don't Move | 3. Reach into your pockets \n");
+    scanf("%d", &choice);
+    if(choice == 1)
+    {
+        C1();
+    }
+    else if(choice == 2)
+    {
+        C2();
+    }
+    else
+    {
+        C3();
+    }
+}
+
+void C1()
+{
+    printf("You walk around and find the room to have a door and a table. \n");
+    printf("1. Open the door | 2. Go to the table \n");
+}
+
+void C2()
+{
+    int ret;
+    char status;
+    printf("You stand still. Regretably, a monster was following you. Tentacles come into view, strangling you. Game Over. \n");
+    printf("Restart? Type Y \n");
+    scanf(" %c", &status);
+    ret = strcmp(&status, "Y");
+    if(ret == 0)
+    {
+        StartPoint();
+    }
+}
+
+void C3()
+{
+
+}
